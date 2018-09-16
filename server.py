584d7246d58c86151131ef39671c6afe89787f58
Peter Gottesman <peter@petergottesman.com>
2018-09-16 04:01:15 -0400

    Completed basic GET portion of API

--- server.py
+++ server.py
@@ -15,25 +15,47 @@ conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+
 def homepage():
     return "Tristan Wiley is a fake"
 
-@app.route("/test", methods=["GET"])
-def test():
+def user(uid):
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES")
+    cursor.execute("SELECT * from users WHERE uid = '{}'".format(uid))
+    row = cursor.fetchone()
+    if row is None:
+        return None
+    user = {
+        "uid": row[0],
+        "firstname": row[1],
+        "lastname": row[2],
+        "email": row[3],
+        "photo": row[4],
+        "bio": row[5],
+        "rating": row[5],
+        "td_id": row[6]
+        }
+    return user
+
+def bids(buy):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM bids WHERE buy = '{}'".format(buy))
     rows = cursor.fetchall()
-    if rows is None:
-        return "Empty"
-    return str([el for el in [row for row in rows]])
+    output = []
+    for row in rows:
+        output.append( {
+            "id" : row[0],
+            "buy" : row[1],
+            "price" : row[2],
+            "location" : row[3],
+            "user" : user(row[4]),
+            "timestamp" : str(row[5])
+        })
+    return output
 
 @app.route("/buys", methods=["GET"])
 def buys():
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM bids WHERE buy = 1")
-    rows = cursor.fetchall()
-    return str(rows)
-
+    return str(bids(True))
+        
 @app.route("/sells", methods=["GET"])
 def sells():
-    pass
+    return str(bids(False))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
