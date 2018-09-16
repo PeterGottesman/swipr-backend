c4a02a2135b2392537e877f080357a739b1e9632
Peter Gottesman <peter@petergottesman.com>
2018-09-16 06:44:28 -0400

    Kinda added messaging

--- server.py
+++ server.py
@@ -62,6 +62,60 @@ def sells():
 def pubUser(uid):
     return json.dumps(user(uid))
 
+@app.route("/messaging/open", methods=["POST"])
+def openMsgs():
+    in_json = request.get_json()
+    trans = in_json["transaction"]
+    user = in_json["user"]
+    
+
+@app.route("/messaging/send",methods=["POST"])
+def sendMsg():
+    in_json = request.get_json()
+    sender = int(in_json["sender"])
+    parter = int(in_json["parter"])
+    content = in_json["content"]
+    cursor = conn.cursor()
+    cursor.execute("""INSERT INTO chat VALUES ('{u1}', '{u2}', null)
+WHERE NOT EXISTS
+(SELECT id FROM chat WHERE (user1 = {u1} AND user2 = {u2}) OR (user1 = {u2} AND user2 = {u1}))"""
+    .format(u1=sender, u2=parter))
+
+    cursor.commit()
+    
+    cursor.execute("""
+INSERT INTO messages
+VALUES ({sender}, (SELECT id FROM chat WHERE (user1 = {u1} AND user2 = {u2}) OR (user1 = {u2} AND user2 = {u1})),
+ '{content}', GETDATE())"""
+                   .format(sender=sender, u1=sender, u2=parter, content=content))
+    cursor.commit()
+    return getMessages(sender, parter)
+    
+                    
+@app.route("/messaging/receive/<u1>/<u2>", methods=["GET"])
+def getMessages(u1, u2):
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * from messages mess
+INNER JOIN
+(
+SELECT id FROM chat WHERE (user1 = {u2} AND user2 = {u1})
+OR (user1 = {u1} AND user2 = {u2})
+) chats
+ON chats.id = mess.chat
+"""
+                   .format(u1=u1, u2=u2))
+    messages = cursor.fetchall()
+    output = []
+    for message in messages:
+        output.append({
+            "id": message[0],
+            "sender": user(message[1]),
+            "chat": message[2],
+            "content": message[3],
+            "timestamp": str(message[4])
+            })
+    return json.dumps(output)
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
